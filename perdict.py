8619ecbe6118c9af7e82d396eedebd47badc7788
Huang-rookie <81640703+Huang-rookie@users.noreply.github.com>
2022-01-14 02:50:05 +0300

    Update perdict.py

--- perdict.py
+++ perdict.py
@@ -18,7 +18,7 @@ def main():
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     # load image
-    img_path = "D:/python/practice/vgg/data_set/flower_data/train/tulips"
+    img_path = "../tulip.jpg"
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
     img = Image.open(img_path)
     plt.imshow(img)
